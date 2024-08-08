b51c81602634971d81f8531ee69043c7de55b598
intezya <intezya@gmail.com>
2024-08-08 16:29:49 +0400

    Add struct constructor (from bytes)

--- src/uuid_struct.rs
+++ src/uuid_struct.rs
@@ -1,7 +1,12 @@
-use pyo3::{pyclass, pymethods};
+use pyo3::{Bound, pyclass, pymethods, PyResult};
+use pyo3::prelude::PyAnyMethods;
+use pyo3::types::PyBytes;
 use uuid::Uuid;
+use uuid::Bytes;
+
 
 #[pyclass(subclass, module="uuid_lib")]
+#[derive(Clone)]
 pub struct UUID {
     pub uuid: Uuid
 }
@@ -9,6 +14,18 @@ pub struct UUID {
 
 #[pymethods]
 impl UUID {
+    #[new]
+    fn new(
+        bytes: &Bound<'_, PyBytes>,
+    ) -> PyResult<Self> {
+
+        let uuid = match bytes {
+            bytes => Self::new_from_bytes(bytes),
+        }?;
+        Ok(uuid)
+    }
+
+
     fn __str__(&self) -> String {
         return self.uuid.hyphenated().to_string();
     }
@@ -22,4 +39,11 @@ impl UUID {
         self.uuid.as_bytes()
     }
 
+    #[staticmethod]
+    fn new_from_bytes(bytes: &Bound<'_, PyBytes>) -> PyResult<UUID> {
+        let bytes: Bytes = bytes.extract()?;
+        Ok(UUID {
+            uuid: Uuid::from_bytes(bytes),
+        })
+    }
 }
